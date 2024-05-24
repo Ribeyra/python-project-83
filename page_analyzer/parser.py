2fce9413cba411b13fc1d6a8b2eb45edcaa59d89
ribeyra <ribeyra@gmail.com>
2024-05-24 12:53:15 +0300

    add class Parser

--- page_analyzer/parser.py
+++ page_analyzer/parser.py
@@ -13,3 +13,29 @@ def get_site_info(html_doc):
         if 'content' in str(raw_description) else ''
 
     return h1, title, description
+
+# alt
+
+
+class Parser:
+    def __init__(self, html_doc):
+        self.soup = BeautifulSoup(html_doc.text, 'html.parser')
+
+    def get_h1(self):
+        if self.soup.h1:
+            return self.soup.h1.text
+        return ''
+
+    def get_title(self):
+        if self.soup.title:
+            return self.soup.title.string
+        return ''
+
+    def get_description(self):
+        raw_description = self.soup.find(attrs={"name": "description"})
+        if 'content' in str(raw_description):
+            return raw_description['content']
+        return ''
+
+    def get_site_info(self):
+        return self.get_h1, self.get_title, self.get_description
